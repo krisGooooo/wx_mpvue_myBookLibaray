cfc584cf903d633b6554c09e13e8237af6a2ceac
krisGooooo <shenya123@hotmail.com>
2018-12-11 22:37:41 +0800

    choose useful info from api & init book table

--- server/controllers/addbook.js
+++ server/controllers/addbook.js
@@ -1,18 +1,35 @@
-//  获取豆瓣信息，入库
+/*
+ * @Author: krisGooooo
+ * @Description: 获取豆瓣信息，入库
+ * @Date: 2018-12-09 21:10:50
+ * @LastEditors: krisGooooo
+ * @LastEditTime: 2018-12-11 22:36:09
+ */
+
 const https = require('https')
 
 module.exports = async (ctx) => {
-  console.log(ctx.request.body)
   const { isbn, open_id } = ctx.request.body
-  console.log(isbn, open_id)
   if(isbn && open_id){
-    console.log(123)
     let url = 'https://api.douban.com/v2/book/isbn/' + isbn
     const bookInfo = await getJSON(url)
-    console.log(bookInfo)
+    const rate = bookInfo.rating.average
+    const { title, image, alt, publisher, summary, price} = bookInfo
+    const tags = bookInfo.tags.map(v=>{
+      return `${v.title} ${v.count}`
+    }).join(',')
+    const author = bookInfo.author.join(',')
+    console.log({
+      rate, title, image, alt, publisher, summary, price,tags,author
+    })
   }
 }
 
+/**
+ * @msg: 获取豆瓣信息
+ * @param {string}
+ * @return: json
+ */
 function getJSON(url){
   return new Promise((resolve, reject) => {
     https.get(url,res=>{

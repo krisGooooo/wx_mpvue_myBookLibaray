fac738af571613a8f561f3be0540d0a947e868e9
krisGooooo <shenya123@hotmail.com>
2018-12-13 23:09:49 +0800

    init booklist api

--- src/pages/books/Book.vue
+++ src/pages/books/Book.vue
@@ -1,12 +1,26 @@
 <template>
     <div>
-        书的页面
+        <div v-for="book in books" :key="book.id">{{ book.title }}</div>
     </div>
 </template>
 
 <script>
+import {get} from '@/util'
 export default {
-
+    data(){
+        return{
+            books:[]
+        }
+    },
+    methods:{
+        async getList(){
+            const book = await get('/weapp/booklist')
+            this.books = book.list
+        }
+    },
+    mounted(){
+        this.getList()
+    }
 };
 </script>
 

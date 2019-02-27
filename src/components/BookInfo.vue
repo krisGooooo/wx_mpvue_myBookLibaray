7760721cce4df3337514b7fcea1bd01790159666
krisGooooo <shenya123@hotmail.com>
2019-02-27 23:32:04 +0800

    book detail init

---
+++ src/components/BookInfo.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="bookinfo">
+    <div class="thumb">
+      <img 
+        class="back" 
+        :src="info.image"
+        mode="aspectFill"
+        >
+      <img 
+        :src="info.image" 
+        class="img"
+        mode="aspectFit"
+        >
+      <div class="info">
+        <div class="title">
+          {{ info.title }}
+        </div>
+        <div class="author">
+          {{ info.author }}
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+<script>
+export default {
+  props:['info']
+}
+</script>
+<style lang="scss" scoped>
+.bookinfo{
+  .thumb{
+    width: 750rpx;
+    height: 500rpx;
+    overflow: hidden;
+    position: relative;
+    .back{
+      filter: blur(15px);
+      width: 100%;
+
+    }
+    .img{
+      position: absolute;
+      height: 300rpx;
+      width: 100%;
+      left: 0;
+      top: 30rpx;
+    }
+    .info{
+      color: white;
+      position: absolute;
+      width: 100%;
+      left: 0;
+      top: 330rpx;
+      text-align: center;
+      .title{
+        font-size: 20px;
+      }
+      .author{
+        font-size: 14px
+      }
+    }
+  }
+}
+</style>
+
+
--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,17 +1,28 @@
 <template>
-  <div>图书id：{{ bookid }}</div>
+  <div>
+    <BookInfo :info="info"></BookInfo>
+  </div>
 </template>
 <script>
 import {get} from '@/util'
+import BookInfo from '@/components/BookInfo'
 export default {
+  components:{
+    BookInfo
+  },
   data(){
     return{
-      bookid: ''
+      bookid: '',
+      info: {}
     }
   },
   methods:{
     async getDetail() {
       const info = await get('/weapp/bookdetail',{id : this.bookid})
+      wx.setNavigationBarTitle({
+        title: info.title
+      })
+      this.info = info
     }
   },
   mounted(){
